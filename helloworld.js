33f4a545b15105027b0a7b23875d2e9e1d41694a
Monu Verma <92913914+vm1501@users.noreply.github.com>
2021-10-25 21:37:19 +0530

    Create helloworld.js

---
+++ helloworld.js
@@ -0,0 +1,9 @@
+/*   
+Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine.
+Node.js is a JavaScript running on the server.
+*/
+
+// this is a simple program to print something as a string 
+console.log('Welcome to node js tutorial.')
+// another type
+console.log(`hello world!`)
